de8a266c709e8a9d91413e30c0b2120ce0ea68ba
kieranomahony <117429464@umail.ucc.ie>
2021-03-26 11:37:23 +0000

    final commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# How to run this Application: 
+
+1. Install NodeJs on your computer. https://nodejs.org/en/ the LTS version will suffice.
+2. Navigate to the root level of this project within your terminal and run command `npm i`. This is to install project dependencies.
+3. run command `npm run local` This will start the automation side of the application.
+
+This part of the application runs on scheduled functions, if you look in the terminal you will see a sample function getting executed every second.
--- services/movieDbService.js
+++ services/movieDbService.js
@@ -1,14 +1,14 @@
 import { remove } from 'winston';
 import { logger } from '../helpers/logger';
 import MovieSchema from '../mongoModels/movieModel';
-
+import TrendingSchema from '../mongoModels/Trending';
 /**
  * get movie curation for a user
  * @param {String} userId
  */
 export async function getMoviesFromDatabase() {
     try {
-        return await MovieSchema.find({})
+        return await MovieSchema.find({}).lean()
             .then((users) => users);
     } catch (err) {
         logger.error(`failed to retrieve user movies for automation`);
@@ -17,12 +17,32 @@ export async function getMoviesFromDatabase() {
 }
 
 export async function writeToDB(id, results, type) {
-    const playlist = (type === 0) ? 'userPlaylists.weeklyPlaylists' : (type === 1) ? 'userPlaylists.monthlyPlaylists' : 'userPlaylists.allTimePlaylists';
+    if (type === 3) {
+        return await writeTrending(results);
+    }
 
+    const playlist = (type === 0) ? 'userPlaylists.weeklyPlaylists' : (type === 1) ? 'userPlaylists.monthlyPlaylists' : 'userPlaylists.allTimePlaylists';
     return await MovieSchema.updateOne({ _id: id }, { $set: { [playlist]: results } })
         .then((user) => true)
         .catch((err) => {
             logger.error(`Failed to write to database: ${err.message}`);
             throw err;
         });
+}
+
+async function writeTrending(results) {
+    const isDeleted = await TrendingSchema.deleteMany({})
+        .then((deleted) => true)
+        .catch((err) => {
+            logger.error(`failed to delete trending page: ${err.message}`);
+        });
+    if (!isDeleted) {
+        return false;
+    }
+    return await new TrendingSchema(results).save()
+        .then((res) => true)
+        .catch((err) => {
+            logger.error(`Failed to write trending: ${err.message}`);
+            throw err;
+        })
 }
